fa039f5e17295d91d0cb5ab1e8a40073b8d8483c
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-29 20:06:35 +0400

    connection to MongoDB

---
+++ mashart/backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+
+    console.log(`MongoDB Connnected : ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.underline.bold)
+  }
+}
+
+export default connectDB
--- mashart/backend/server.js
+++ mashart/backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+import connectDB from "./config/db.js"
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
