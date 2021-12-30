7e131d6f52e548a345a8117dec55276c3b1151a2
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 17:10:56 +0400

    redux user action to register

--- mashart/frontend/src/actions/userActions.js
+++ mashart/frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants"
 
 export const login = (username, password) => async (dispatch) => {
@@ -42,7 +45,7 @@ export const login = (username, password) => async (dispatch) => {
   }
 }
 
-export const logout = () => async (dispatch) => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo")
   dispatch({
     type: USER_LOGOUT,
@@ -51,3 +54,44 @@ export const logout = () => async (dispatch) => {
     type: USER_DETAILS_RESET,
   })
 }
+
+export const register =
+  (username, email, password, confirmPassword) => async (dispatch) => {
+    try {
+      dispatch({
+        type: USER_REGISTER_REQUEST,
+      })
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+
+      const { data } = await axios.post(
+        "/api/users",
+        {
+          username,
+          email,
+          password,
+          confirmPassword,
+        },
+        config
+      )
+
+      dispatch({
+        type: USER_REGISTER_SUCCESS,
+        payload: data,
+      })
+
+      localStorage.setItem("userInfo", JSON.stringify(data))
+    } catch (error) {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }
