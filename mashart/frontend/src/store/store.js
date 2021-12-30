bae113fdc2843bcb3846d321a01fdb4ed32fd1cb
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 20:21:31 +0400

    added all the user reducers to store

--- mashart/frontend/src/store/store.js
+++ mashart/frontend/src/store/store.js
@@ -1,8 +1,19 @@
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import composeWithDevTools from "redux-devtools-extension"
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from "../reducers/userReducers"
 
-let reducer = combineReducers({})
+let reducer = combineReducers({
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+})
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
