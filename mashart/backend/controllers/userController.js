798b2dc5fc7e7e9f9126d8e1d74bfdcc168a68b1
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 13:25:36 +0400

    userController for registration

--- mashart/backend/controllers/userController.js
+++ mashart/backend/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from "../utils/generateToken.js"
 import User from "../models/userModel.js"
 
 // @desc user auth & get token
-// @route /api/users/login
+// @route POST /api/users/login
 // @access Public
 export const authUser = asyncHandler(async (req, res) => {
   const { username, password } = req.body
@@ -14,10 +14,50 @@ export const authUser = asyncHandler(async (req, res) => {
     res.json({
       _id: user._id,
       username: user.username,
+      email: user.email,
       token: generateToken(user._id),
     })
   } else {
     res.status(401)
     throw new Error("Invalid username or password")
+  }
+})
+
+// @desc register new user
+// @route POST /api/users
+// @access Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    res.status(400)
+    throw new Error("Passwords dont match")
+  }
+
+  const userExists =
+    (await User.findOne({ username })) || (await User.findOne({ email }))
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("User already Exists")
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(200)
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid user data")
   }
 })
