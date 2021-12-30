30a19a8cf50204c74c457f09931bcb0740304936
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 20:19:26 +0400

    redux user reducer to update user profile

--- mashart/frontend/src/reducers/userReducers.js
+++ mashart/frontend/src/reducers/userReducers.js
@@ -10,6 +10,9 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
@@ -50,6 +53,19 @@ export const userDetailsReducer = (state = {}, action) => {
       return { loading: false, error: action.payload }
     case USER_DETAILS_RESET:
       return { user: {} }
+    default:
+      return state
+  }
+}
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userLogin: action.payload }
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
