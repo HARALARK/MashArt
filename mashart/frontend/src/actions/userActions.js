67e7922b5881d35fe1701bb3006b4251eca34bdd
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 16:17:50 +0400

    redux user action for login

---
+++ mashart/frontend/src/actions/userActions.js
@@ -0,0 +1,41 @@
+import axios from "axios"
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants"
+
+export const login = (username, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    })
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { username, password },
+      config
+    )
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      paylaod: data,
+    })
+
+    localStorage.setItem("userInfo", JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
