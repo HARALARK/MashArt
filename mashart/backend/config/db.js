0bf41f6a1a8a9da5980a1fb9a88acb8c2875cce0
Abdul Kader <abdulkaderm36@gmail.com>
2021-12-30 14:11:31 +0400

    removed a console.log

--- mashart/backend/config/db.js
+++ mashart/backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI)
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
