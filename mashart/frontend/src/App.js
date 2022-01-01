ed8b1542c258ef22366994a4cd164acabffd9e48
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-01 16:35:36 +0400

    setting up the  react router for navigation

--- mashart/frontend/src/App.js
+++ mashart/frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
 
 function App() {
-  return <div className="App">MashArt</div>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
