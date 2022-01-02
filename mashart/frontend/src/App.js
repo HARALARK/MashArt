d165185a9049a744b188e0d0de9c56ccefa3b8cf
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-02 21:41:53 +0400

    Added header to the screen

--- mashart/frontend/src/App.js
+++ mashart/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
+import Header from "./components/Header"
 
 function App() {
   return (
     <Router>
+      <Header />
       <div className="App">
         <Routes>
           <Route path="/" />
