fa5a446b3da10f02a9ef83657deca47441e898e8
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-04 15:29:03 +0400

    created another component for the design

---
+++ mashart/frontend/src/components/Design.js
@@ -0,0 +1,85 @@
+import React from "react"
+import styled from "styled-components"
+import device from "../screen_sizes/devices"
+
+const Design = () => {
+  return (
+    <Right>
+      <div className="container">
+        <SVG src="./images/blob.svg" alt="blob" boxSize="320" />
+        <SVG
+          className="collab"
+          src="./images/collab.svg"
+          alt="collab"
+          boxSize="250"
+        />
+        <div className="text-design">
+          <p className="collaborate">Collaborate</p>
+          <p className="share">Share</p>
+          <p className="enjoy">Enjoy!!!</p>
+        </div>
+      </div>
+    </Right>
+  )
+}
+
+const Right = styled.div`
+  .container {
+    position: relative;
+  }
+  .collab {
+    position: absolute;
+    left: 0;
+  }
+
+  .text-design {
+    position: absolute;
+    top: 60%;
+    font-weight: 900;
+    font-size: 2rem;
+    line-height: 65%;
+    letter-spacing: -2px;
+  }
+
+  .text-design .collaborate {
+    color: #ffffff;
+  }
+
+  .text-design .share {
+    color: #7c41a7;
+  }
+
+  .text-design .enjoy {
+    color: #dd4a5c;
+  }
+
+  @media ${device.tablet} {
+    .text-design {
+      font-size: 2.35rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    .text-design {
+      font-size: 2.7rem;
+    }
+  }
+`
+
+const SVG = styled.img`
+  height: ${(props) => props.boxSize || props.height}px;
+  width: ${(props) => props.boxSize || props.width}px;
+  color: ${(props) => props.color || "#DD4A5C"};
+
+  @media ${device.tablet} {
+    height: ${(props) => (props.boxSize || props.height) * 1.2}px;
+    width: ${(props) => (props.boxSize || props.width) * 1.2}px;
+  }
+
+  @media ${device.laptop} {
+    height: ${(props) => (props.boxSize || props.height) * 1.4}px;
+    width: ${(props) => (props.boxSize || props.width) * 1.4}px;
+  }
+`
+
+export default Design
--- mashart/frontend/src/screens/WelcomeScreen.js
+++ mashart/frontend/src/screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import Design from "../components/Design"
 import device from "../screen_sizes/devices"
 
 const WelcomeScreen = () => {
@@ -21,22 +22,7 @@ const WelcomeScreen = () => {
             <Button>Login</Button>
           </div>
         </Left>
-        <Right>
-          <div className="container">
-            <SVG src="./images/blob.svg" alt="blob" boxSize="320" />
-            <SVG
-              className="collab"
-              src="./images/collab.svg"
-              alt="collab"
-              boxSize="250"
-            />
-            <div className="text-design">
-              <p className="collaborate">Collaborate</p>
-              <p className="share">Share</p>
-              <p className="enjoy">Enjoy!!!</p>
-            </div>
-          </div>
-        </Right>
+        <Design />
       </Container>
     </Hero>
   )
@@ -110,49 +96,6 @@ const Left = styled.div`
   }
 `
 
-const Right = styled.div`
-  .container {
-    position: relative;
-  }
-  .collab {
-    position: absolute;
-    left: 0;
-  }
-
-  .text-design {
-    position: absolute;
-    top: 60%;
-    font-weight: 900;
-    font-size: 2rem;
-    line-height: 65%;
-    letter-spacing: -2px;
-  }
-
-  .text-design .collaborate {
-    color: #ffffff;
-  }
-
-  .text-design .share {
-    color: #7c41a7;
-  }
-
-  .text-design .enjoy {
-    color: #dd4a5c;
-  }
-
-  @media ${device.tablet} {
-    .text-design {
-      font-size: 2.35rem;
-    }
-  }
-
-  @media ${device.laptop} {
-    .text-design {
-      font-size: 2.7rem;
-    }
-  }
-`
-
 const Button = styled.a`
   display: inline-block;
   width: 100px;
@@ -174,22 +117,6 @@ const Button = styled.a`
     border-bottom: 3px solid #24003e;
     border-radius: 0;
     color: #24003e;
-  }
-`
-
-const SVG = styled.img`
-  height: ${(props) => props.boxSize || props.height}px;
-  width: ${(props) => props.boxSize || props.width}px;
-  color: ${(props) => props.color || "#DD4A5C"};
-
-  @media ${device.tablet} {
-    height: ${(props) => (props.boxSize || props.height) * 1.2}px;
-    width: ${(props) => (props.boxSize || props.width) * 1.2}px;
-  }
-
-  @media ${device.laptop} {
-    height: ${(props) => (props.boxSize || props.height) * 1.4}px;
-    width: ${(props) => (props.boxSize || props.width) * 1.4}px;
   }
 `
 
