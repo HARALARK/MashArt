6376403af806c0f84676ed49602d6bd50bdb8462
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-04 18:45:01 +0400

    created styled components

---
+++ mashart/frontend/src/components/styled-components/Input.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Input = styled.input`
+  padding: 0.7rem 1rem;
+  border: none;
+  border-radius: 5px;
+  width: 300px;
+`
+
+export default Input
---
+++ mashart/frontend/src/components/styled-components/Message.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+const Message = styled.p`
+  background-color: ${(props) =>
+    (props.variant === "success" && "#D1E7DD") ||
+    (props.variant === "error" && "#F8D7DA") ||
+    (props.variant === "warning" && "#FFF3CD") ||
+    "#3E3E3E"};
+
+  color: ${(props) =>
+    (props.variant === "success" && "#2B674B") ||
+    (props.variant === "error" && "#9C464D") ||
+    (props.variant === "warning" && "#664D03") ||
+    "#F0F0F0"};
+
+  font-size: 0.8rem;
+
+  padding: 0.5rem 1rem;
+  margin-bottom: 1rem;
+  border-radius: 5px;
+
+  width: 300px;
+`
+
+export default Message
