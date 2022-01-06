0eac7b6e34d7eaaaed7edea289346c0a35d3dcd4
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-06 17:37:32 +0400

    added the alignment styling to Home screen only

--- mashart/frontend/src/screens/HomeScreen.js
+++ mashart/frontend/src/screens/HomeScreen.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import styled from "styled-components"
 import WelcomeScreen from "./WelcomeScreen"
 
 const Homepage = () => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
-  return userInfo ? <div>Home Screen</div> : <WelcomeScreen />
+  return userInfo ? <Container>Home Screen</Container> : <WelcomeScreen />
 }
+
+const Container = styled.section`
+  padding: 1rem 2rem;
+  height: calc(100vh - 160px);
+`
 
 export default Homepage
