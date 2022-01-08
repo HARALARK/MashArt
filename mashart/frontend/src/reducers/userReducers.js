5b69415d295b31950157ef038160d71d65c51bd4
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-08 14:36:08 +0400

    changed userInfo to user in userDetails due to conflicts

--- mashart/frontend/src/reducers/userReducers.js
+++ mashart/frontend/src/reducers/userReducers.js
@@ -48,7 +48,7 @@ export const userDetailsReducer = (state = {}, action) => {
     case USER_DETAILS_REQUEST:
       return { ...state, loading: true }
     case USER_DETAILS_SUCCESS:
-      return { loading: false, userInfo: action.payload }
+      return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case USER_DETAILS_RESET:
