ccc3ddc862bd35a184af4f721443e1154d249b95
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-08 14:39:55 +0400

    added user to initial state

--- mashart/frontend/src/reducers/userReducers.js
+++ mashart/frontend/src/reducers/userReducers.js
@@ -43,7 +43,7 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 }
 
-export const userDetailsReducer = (state = {}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
       return { ...state, loading: true }
