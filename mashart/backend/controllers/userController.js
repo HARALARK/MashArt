34dcc8d0e5024343348d5d8fd20e0f123d3eee22
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-10 14:40:36 +0400

    fixed typo for password

--- mashart/backend/controllers/userController.js
+++ mashart/backend/controllers/userController.js
@@ -10,7 +10,7 @@ export const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ username })
 
-  if (user && user.matchPassword(password)) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       username: user.username,
