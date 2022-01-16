3394efe619167f14563accf6e6ad04898968eb52
AloraTab <aat2000@hw.ac.uk>
2022-01-16 21:57:46 +0400

    Initial setup

---
+++ mashart/frontend/src/screens/CollabScreen.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+
+const CollabScreen = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    
+}
