5f54fa0c0e99b88ac974d46c56a117e992a0d6f4
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-17 16:56:29 +0400

    added ForgotPassword route

--- mashart/frontend/src/App.js
+++ mashart/frontend/src/App.js
@@ -5,6 +5,7 @@ import "./App.css"
 import Footer from "./components/Footer"
 
 import Header from "./components/Header"
+import ForgotPasswordScreen from "./screens/ForgotPasswordScreen"
 import HomeScreen from "./screens/HomeScreen"
 import LoginScreen from "./screens/LoginScreen"
 import ProfileScreen from "./screens/ProfileScreen"
@@ -22,6 +23,10 @@ function App() {
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/login" element={<LoginScreen />} />
               <Route path="/profile" element={<ProfileScreen />} />
+              <Route
+                path="/forgot-password"
+                element={<ForgotPasswordScreen />}
+              />
             </Routes>
           </ScreensContainer>
         </AppContainer>
