edc499da6e681d06259857cd020b11614df1dffc
AloraTab <aat2000@hw.ac.uk>
2022-01-23 13:20:04 +0400

    Added tabs component with styling

--- mashart/frontend/src/components/TabComponent/Tabs.js
+++ mashart/frontend/src/components/TabComponent/Tabs.js
@@ -37,4 +37,107 @@ const Tabs = () => {
     );
 };
 
+<<<<<<< HEAD
 export default Tabs;
+=======
+export default Tabs;
+
+
+
+
+
+// ---- OLD version, KEPT FOR REFERENCE ---
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from "styled-components"
+// import Tab from './Tab';
+
+
+
+// class Tabs extends Component {
+//   static propTypes = {
+//     children: PropTypes.instanceOf(Array).isRequired,
+//   }
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       activeTab: this.props.children[0].props.label,
+//     };
+//   }
+
+//   onClickTabItem = (tab) => {
+//     this.setState({ activeTab: tab });
+//   }
+
+
+//   render() {
+//     const {
+//       onClickTabItem,
+//       props: {
+//         children,
+//       },
+//       state: {
+//         activeTab,
+//       }
+//     } = this;
+
+//     return (
+//     <Container>
+//       <div className="tabs">
+//         <ol className="tab-list">
+//           {children.map((child) => {
+//             const { label } = child.props;
+
+//             return (
+//               <Tab
+//                 activeTab={activeTab}
+//                 key={label}
+//                 label={label}
+//                 onClick={onClickTabItem}
+//               />
+//             );
+//           })}
+//         </ol>
+//         <div className="tab-content">
+//           {children.map((child) => {
+//             if (child.props.label !== activeTab) return undefined;
+//             return child.props.children;
+//           })}
+//         </div>
+//       </div>
+//       </Container>
+//     );
+//   }
+// }
+
+// const Container = styled.div`
+    
+
+//       .tab-list {  
+//         border-bottom: 1px solid #ccc;
+//         padding-left: 0;
+//         list-style: circle;
+//       }
+      
+//       .tab-list-item {
+//         display: inline;
+//         margin-bottom: -1px;
+//         padding: 0.5rem 0.75rem;
+//       }
+      
+//       .tab-list-active {
+//         display: inline-block;
+//         font-weight: bold;
+//         border: solid #ccc;
+//         border-width: 1px 1px 0 1px;
+//       }
+
+//       .tab-content {
+          
+//       }
+// `
+
+// export default Tabs;
+>>>>>>> 0f52738 (Added tabs component with styling)
--- mashart/frontend/src/screens/CollabScreen.js
+++ mashart/frontend/src/screens/CollabScreen.js
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 import React, {useState} from "react"
 import {useDispatch, useSelector } from "react-redux";
 // import Tabs from "../components/TabComponent/Tabs";
 import styled from "styled-components"
 import Message from "../components/styled-components/Message"
+=======
+import React from "react"
+import Tabs from "../components/TabComponent/Tabs";
+import styled from "styled-components"
+
+>>>>>>> 0f52738 (Added tabs component with styling)
 import device from "../screen_sizes/devices"
 import { Input } from "../components/styled-components/Input";
 import { Link } from "react-router-dom";
@@ -18,6 +25,7 @@ const CollabScreen = () => {
     const { loading, error } = userLogin
 
     return (
+<<<<<<< HEAD
         <Hero>
             <Container>
                 
@@ -40,10 +48,26 @@ const CollabScreen = () => {
                 </div>
             </Container>
         </Hero>
+=======
+        <>
+            
+            <CreateCollab>
+                <CollabHolder> 
+                    <p className="heading"> Create your own Collaborate Project </p>
+                </CollabHolder>
+            </CreateCollab>
+            <Container> 
+                
+                <Tabs />
+            </Container>
+
+        </>
+>>>>>>> 0f52738 (Added tabs component with styling)
         
     ) 
 }
 
+<<<<<<< HEAD
 const Hero = styled.section`
   width: 100%;
   display: flex;
@@ -53,6 +77,18 @@ const Hero = styled.section`
 const Divider = styled.section`
   outline: 1px dashed white;
 `
+=======
+const CreateCollab = styled.div`
+
+`
+const PostsDisplay = styled.div `
+    display: flex;
+    color: white;
+    height: 100%;
+    width: 50%;
+`
+
+>>>>>>> 0f52738 (Added tabs component with styling)
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -76,9 +112,25 @@ const Container = styled.div`
     gap: 2rem;
     padding: 0 2rem;
 
+<<<<<<< HEAD
   }
 
 `
+=======
+        .design-container {
+            display: inline-block;
+        }
+    }
+    
+    outline: 1px dashed red;  For debugging
+`
+
+const CollabHolder = styled.div`
+background-color: #EBDFCE;
+padding: 1rem 2rem;
+align-items: inline;
+border-radius: 5px;
+>>>>>>> 0f52738 (Added tabs component with styling)
 
 const Form = styled.form`
   background-color: var(--secondary-dark);
@@ -93,6 +145,7 @@ const Form = styled.form`
 
 `
 
+<<<<<<< HEAD
 const SubmitButton = styled(Input)`
   background-color: var(--primary);
   color: var(--dark);
@@ -107,4 +160,6 @@ const SubmitButton = styled(Input)`
   }
 `
 
+=======
+>>>>>>> 0f52738 (Added tabs component with styling)
 export default CollabScreen;
