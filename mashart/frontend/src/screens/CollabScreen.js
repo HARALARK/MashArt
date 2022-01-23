23cb786918f93b434987d9d19dc741e5c9253bb1
AloraTab <aat2000@hw.ac.uk>
2022-01-23 14:58:57 +0400

    Initial creation of the screen.

--- mashart/frontend/src/screens/CollabScreen.js
+++ mashart/frontend/src/screens/CollabScreen.js
@@ -1,77 +1,110 @@
-import React from "react"
+import React, {useState} from "react"
+import { useDispatch, useSelector } from "react-redux";
 import Tabs from "../components/TabComponent/Tabs";
 import styled from "styled-components"
-
+import Message from "../components/styled-components/Message"
 import device from "../screen_sizes/devices"
+import { Input } from "../components/styled-components/Input";
+
+
+
 const CollabScreen = () => {
     
 
-    return (
-        <>
-            
-            <CreateCollab>
-                <CollabHolder> 
-                    <p className="heading"> Create your own Collaborate Project </p>
-                </CollabHolder>
-            </CreateCollab>
-            <Container> 
-                
-                <Tabs />
-            </Container>
+    const [roomCode] = useState("")
 
-        </>
+    const userLogin = useSelector((state) => state.userLogin)
+    const { loading, userInfo, error } = userLogin
+
+    return (
+        <Hero>
+            <Container>
+                
+                <div className="join-container">
+                    <Form>
+                        <p className="heading">Collab</p>
+                        {loading && <Message>Loading...</Message>}
+                        {error && <Message variant="error">{error}</Message>}
+                        <SubmitButton type = "button" value="Start Room"  />
+
+
+                        <Input
+                            type="text"
+                            placeholder="Room Code"
+                            // Need something here to search for valid room
+                            required
+                        />
+
+                        <SubmitButton type = "button" value="Join Room"  />
+                    </Form>
+                </div>
+            </Container>
+        </Hero>
         
     ) 
 }
 
-const CreateCollab = styled.div`
-
-`
-const PostsDisplay = styled.div `
-    display: flex;
-    color: white;
-    height: 100%;
-    width: 50%;
+const Hero = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Container = styled.div`
-    display: flex;
-    align-items: inline;
-    // overflow-y: scroll;
-    // overflow-style: none;
-    height: calc(100vh - 80px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    .design-container {
+  height: calc(100vh - 80px);
+
+  .design-container {
     display: none;
-    }
-    
-    @media ${device.laptop} {
+  }
+
+  .heading {
+    color: var(--light);
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  @media ${device.laptop} {
     flex-direction: row;
     height: calc(100vh - 80px);
     justify-content: space-between;
     gap: 2rem;
-    padding: 3rem 2rem;
+    padding: 0 2rem;
 
-        .design-container {
-            display: inline-block;
-        }
-    }
-    
-    outline: 1px dashed red;  For debugging
+  }
+
+  outline: 1px dashed red;
 `
 
-const CollabHolder = styled.div`
-background-color: #EBDFCE;
-padding: 1rem 2rem;
-align-items: inline;
-border-radius: 5px;
+const Form = styled.form`
+  background-color: var(--secondary);
+  padding: 2rem 2rem;
+  border-radius: 5px;
 
-height: 40%;
-width: 100%;
-display: flex;
-flex-direction: column;
-gap: 1.2rem;
-outline: 1px dashed green;
+  width: 350px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  outline: 1px dashed black;
+`
+
+const SubmitButton = styled(Input)`
+  background-color: var(--primary);
+  color: var(--light);
+  font-weight: 600;
+  margin: 0rem 0 0rem;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--primary-dark);
+  }
 `
 
 export default CollabScreen;
