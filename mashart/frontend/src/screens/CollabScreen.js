0e9915fdc829e7145d4d5b0e42236545f45426f3
AloraTab <aat2000@hw.ac.uk>
2022-01-23 13:20:04 +0400

    Added tabs component with styling

--- mashart/frontend/src/screens/CollabScreen.js
+++ mashart/frontend/src/screens/CollabScreen.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+
 import React, {useState} from "react"
 import {useDispatch, useSelector } from "react-redux";
 // import Tabs from "../components/TabComponent/Tabs";
 import styled from "styled-components"
 import Message from "../components/styled-components/Message"
-=======
 import React from "react"
 import Tabs from "../components/TabComponent/Tabs";
 import styled from "styled-components"
-
->>>>>>> 0f52738 (Added tabs component with styling)
+import React from "react"
+import Tabs from "../components/TabComponent/Tabs";
+import styled from "styled-components"
 import device from "../screen_sizes/devices"
 import { Input } from "../components/styled-components/Input";
 import { Link } from "react-router-dom";
@@ -17,7 +17,7 @@ import { Link } from "react-router-dom";
 
 
 const CollabScreen = () => {
-    
+  
 
     const [roomCode] = useState("")
 
@@ -25,7 +25,6 @@ const CollabScreen = () => {
     const { loading, error } = userLogin
 
     return (
-<<<<<<< HEAD
         <Hero>
             <Container>
                 
@@ -48,26 +47,11 @@ const CollabScreen = () => {
                 </div>
             </Container>
         </Hero>
-=======
-        <>
-            
-            <CreateCollab>
-                <CollabHolder> 
-                    <p className="heading"> Create your own Collaborate Project </p>
-                </CollabHolder>
-            </CreateCollab>
-            <Container> 
-                
-                <Tabs />
-            </Container>
 
-        </>
->>>>>>> 0f52738 (Added tabs component with styling)
         
     ) 
 }
 
-<<<<<<< HEAD
 const Hero = styled.section`
   width: 100%;
   display: flex;
@@ -77,18 +61,7 @@ const Hero = styled.section`
 const Divider = styled.section`
   outline: 1px dashed white;
 `
-=======
-const CreateCollab = styled.div`
 
-`
-const PostsDisplay = styled.div `
-    display: flex;
-    color: white;
-    height: 100%;
-    width: 50%;
-`
-
->>>>>>> 0f52738 (Added tabs component with styling)
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -111,26 +84,7 @@ const Container = styled.div`
     justify-content: space-between;
     gap: 2rem;
     padding: 0 2rem;
-
-<<<<<<< HEAD
-  }
-
 `
-=======
-        .design-container {
-            display: inline-block;
-        }
-    }
-    
-    outline: 1px dashed red;  For debugging
-`
-
-const CollabHolder = styled.div`
-background-color: #EBDFCE;
-padding: 1rem 2rem;
-align-items: inline;
-border-radius: 5px;
->>>>>>> 0f52738 (Added tabs component with styling)
 
 const Form = styled.form`
   background-color: var(--secondary-dark);
@@ -145,7 +99,6 @@ const Form = styled.form`
 
 `
 
-<<<<<<< HEAD
 const SubmitButton = styled(Input)`
   background-color: var(--primary);
   color: var(--dark);
@@ -159,7 +112,4 @@ const SubmitButton = styled(Input)`
     background-color: var(--primary-dark);
   }
 `
-
-=======
->>>>>>> 0f52738 (Added tabs component with styling)
 export default CollabScreen;
