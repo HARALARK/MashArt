b3df54a8b3f54d2411973ea43b5737695f49a0e0
rgs2000 <71346259+rgs2000@users.noreply.github.com>
2022-01-27 09:18:58 +0400

    added styling for profile picture, username and time

--- mashart/frontend/src/screens/HomeScreen.js
+++ mashart/frontend/src/screens/HomeScreen.js
@@ -7,13 +7,15 @@ const Homepage = () => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+
   return userInfo ? (
     <Hero> 
       <Container>
-        <PostSection> 
+        {/* <PostSection>  */}
           <PostUserID> 
           {/* img for profile picture */}
-            <span className = "userName">  Samantha.smith</span>
+            <img className= "profilePicture" src="/images/logo/logo.png" alt="profilepic" /> 
+            <span className = "userName">  samantha.smith</span>
             <span className = "PostTime"> 10 mins ago </span>
           </PostUserID>
 
@@ -26,7 +28,7 @@ const Homepage = () => {
             </PostIcons>
           </PostCaption>
 
-        </PostSection>
+        {/* </PostSection> */}
         
 
 
@@ -43,30 +45,42 @@ const Homepage = () => {
 const Hero = styled.section`
    width: 100%;
    border-radius: 20px;
-   
-   -webkit-box-shadow: 0px 0px 16px -8px rgba(0, 0, 0, 0.68)
-   //margin: 40px 0;
+   box-shadow: 0px 0px 16px -8px rgba(0,0,0,0.40);
+   -webkit-box-shadow: -8px 6px 16px 0px rgba(0,0,0,0.40);
+    margin: 40px 0;
    
 
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
+//    display: flex;
+//    justify-content: center;
+//    align-items: center;
 `
 const Container = styled.section`
-    margin-top: 40 px;
+    //margin-top: 40 px;
     padding: 1rem 2rem;
 `
-const PostSection = styled.section`
-    width : 100%;
-
-
-  
-`
+// const PostSection = styled.section`
+//     width : 100%;
+// `
 const PostUserID = styled.section`
     padding: 10px;
+    display: flex;
+    //justify-content: space-between;
 
-  
+    .profilePicture {
+        height: 35px;
+        width: 35px;
+        background-color: #ffe6aa;
+        border-radius: 45px;
+        padding: 5px; 
+        margin-right: 10px;
+    }
+
+    .PostTime{
+        margin-left: 530px;
+    }
 `
+
+
 // const userName = styled.section`
 //  // padding: 1rem 2rem;
   
