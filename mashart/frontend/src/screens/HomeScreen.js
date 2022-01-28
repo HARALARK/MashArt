f49fa062a96f181dae6ac0fe6d5b8b5da642b098
rgs2000 <71346259+rgs2000@users.noreply.github.com>
2022-01-28 11:42:42 +0400

    added collaborator section

--- mashart/frontend/src/screens/HomeScreen.js
+++ mashart/frontend/src/screens/HomeScreen.js
@@ -18,7 +18,13 @@ const Homepage = () => {
             <span className = "PostTime"> 10 mins ago </span>
           </PostUserID>
 
-          <PostCollaborators> </PostCollaborators>
+          <PostCollaborators> 
+            <img className= "collab1" src="/images/logo/logo.png" alt="collab1" />
+            <img className= "collab2" src="/images/logo/logo.png" alt="collab2" />
+            <img className= "collab3" src="/images/logo/logo.png" alt="collab3" />
+
+
+          </PostCollaborators>
           <PostPicture> 
           <img className= "picturePost" src="/images/samplepainting.PNG" alt="postpic" /> 
           </PostPicture>
@@ -54,7 +60,6 @@ const Hero = styled.section`
     background-color: var(--primary);
     
    
-
 //    display: flex;
 //    justify-content: center;
 //    align-items: center;
@@ -62,7 +67,6 @@ const Hero = styled.section`
 const Container = styled.section`
     //margin-top: 40 px;
     padding: 1rem 2rem;
-
 `
 // const PostSection = styled.section`
 //     width : 100%;
@@ -74,8 +78,8 @@ const PostUserID = styled.section`
     background-color: var(--primary-light);
     border-radius: 20px;
     font-size: 15px;
+    margin-left: 25px;
     
-
     .profilePicture {
         height: 35px;
         width: 35px;
@@ -84,7 +88,6 @@ const PostUserID = styled.section`
         padding: 5px; 
         margin-right: 10px;
     }
-
     .PostTime{
         margin-left: 530px;
     }
@@ -100,7 +103,42 @@ const PostUserID = styled.section`
   
 // `
 const PostCollaborators = styled.section`
- // padding: 1rem 2rem;
+    padding: 10px;
+    display: flex;
+    background-color: var(--primary-light);
+    border-radius: 20px;
+    float: left;
+
+    .collab1{
+        height: 35px;
+        width: 35px;
+        background-color: #ffe6aa;
+        border-radius: 45px;
+        padding: 5px; 
+        // margin-right: 10px;
+        // float: left;
+
+    }
+    .collab2{
+        height: 35px;
+        width: 35px;
+        background-color: #ffe6aa;
+        border-radius: 45px;
+        padding: 5px; 
+        // margin-right: 10px;
+       // float: left;
+
+    }
+    .collab3{
+        height: 35px;
+        width: 35px;
+        background-color: #ffe6aa;
+        border-radius: 45px;
+        padding: 5px; 
+        // margin-right: 10px;
+       // float: left;
+
+    }
   
 `
 const PostPicture = styled.section`
@@ -109,7 +147,6 @@ const PostPicture = styled.section`
     border-radius: 45px;
     padding: 5px; 
     // margin-right: 800px;
-
     .picturePost{
         //background-color: #caf0f8;
         margin-left: 25px;
@@ -117,7 +154,6 @@ const PostPicture = styled.section`
         width: 350px;
         border-radius: 15px;
     }
-
   
 `
 const PostCaption = styled.section`
