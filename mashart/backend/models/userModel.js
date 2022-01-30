29eba077857bba930813dfe24c0641bc0dfa98a7
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-31 01:52:09 +0400

    updated model to also store post's id

--- mashart/backend/models/userModel.js
+++ mashart/backend/models/userModel.js
@@ -17,6 +17,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    posts: {
+      type: Array,
+      default: [],
+    },
     followers: {
       type: Array,
       default: [],
