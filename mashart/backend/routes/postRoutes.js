735065081f0ddbadf4d18046e3a25420d4e4d30c
Abdul Kader <abdulkaderm36@gmail.com>
2022-01-31 09:59:30 +0400

    route to get details for a single post

--- mashart/backend/routes/postRoutes.js
+++ mashart/backend/routes/postRoutes.js
@@ -1,6 +1,10 @@
 import express from "express"
 import multer from "multer"
-import { createPost, updatePost } from "../controllers/postController.js"
+import {
+  createPost,
+  getPostDetails,
+  updatePost,
+} from "../controllers/postController.js"
 
 import { protect } from "../middleware/authMiddleware.js"
 
@@ -9,6 +13,7 @@ const upload = multer({ dest: "backend/uploads/" })
 const router = express.Router()
 
 router.route("/create").post([protect, upload.single("image")], createPost)
+router.route("/post/:id").get(protect, getPostDetails)
 router.route("/update").put(protect, updatePost)
 
 export default router
