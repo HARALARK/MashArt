5f715ace73c8082f806f16533621c0aa705072ef
Abdul Kader <abdulkaderm36@gmail.com>
2022-02-01 18:25:27 +0400

    get posts action and reducer

--- mashart/frontend/src/actions/postActions.js
+++ mashart/frontend/src/actions/postActions.js
@@ -3,6 +3,9 @@ import {
   CREATE_POST_FAIL,
   CREATE_POST_REQUEST,
   CREATE_POST_SUCCESS,
+  GET_POSTS_FAIL,
+  GET_POSTS_REQUEST,
+  GET_POSTS_SUCCESS,
   POST_RESET,
 } from "../constants/postContants"
 
@@ -44,4 +47,38 @@ export const createPostReset = () => async (dispatch) => {
   dispatch({
     type: POST_RESET,
   })
+}
+
+export const getPosts = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: GET_POSTS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get("/api/post/", config)
+
+    dispatch({
+      type: GET_POSTS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: GET_POSTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
