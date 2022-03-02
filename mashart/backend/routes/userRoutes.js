ef057933044bf67458d8fba5cfc22f318b30784b
Abdul Kader <abdulkaderm36@gmail.com>
2022-03-02 23:29:01 +0400

    added profile image

--- mashart/backend/routes/userRoutes.js
+++ mashart/backend/routes/userRoutes.js
@@ -1,4 +1,6 @@
 import express from "express"
+import multer from "multer"
+import path from "path"
 import {
   authUser,
   registerUser,
@@ -16,6 +18,27 @@ import {
 } from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
+const imageStorage = multer.diskStorage({
+  destination: "backend/uploads",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    )
+  },
+})
+
+const upload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg|webp)$/)) {
+      // upload only png and jpg format
+      return cb(new Error("Please upload a Image"))
+    }
+    cb(undefined, true)
+  },
+})
+
 const router = express.Router()
 
 router.route("/").post(registerUser)
@@ -27,7 +50,7 @@ router.route("/email").post(checkEmail)
 router
   .route("/profile")
   .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+  .put([protect, upload.single("profileImage")], updateUserProfile)
   .delete(protect, deleteUser)
 
 router.route("/forgot-password").put(forgotPassword)
