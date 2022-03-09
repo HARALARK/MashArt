98061cef98f468b17d820a2d7bb189edb3d99fd3
Rida <raqp2000@hw.ac.uk>
2022-03-09 05:55:31 +0400

    adding routes to update playlist visibility, add/delete posts and tags, delete playlist, etc.

--- mashart/backend/controllers/playlistController.js
+++ mashart/backend/controllers/playlistController.js
@@ -41,10 +41,10 @@ export const createPlaylist = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Update playlist visibility
+// @desc Update playlist visibility to public
 // @route PUT /api/playlist/:id/public
 // @access Private
-export const changeVisibility = asyncHandler(async (req, res) => {
+export const publicVisibility = asyncHandler(async (req, res) => {
   try{
     const user = await User.findById(req.user._id)
     const playlist = await Playlist.findById(req.params.id)
@@ -56,13 +56,13 @@ export const changeVisibility = asyncHandler(async (req, res) => {
       res.status(404).json("Playlist not found.");
     }
 
-    if(playlist.visibility.equals(false)){
+    if(playlist.visibility.equals(true)){
       res.status(400).json("Invalid Request: Playlist is already public.");
     }
     //check if playlist belongs to user
     if (req.user._id.equals(playlist.userId)){
       await playlist.updateOne({
-        $set: {visibility: false}
+        $set: {visibility: true}
     })
     }
     else{
@@ -75,4 +75,231 @@ export const changeVisibility = asyncHandler(async (req, res) => {
 })
 
 
-  
+// @desc Update playlist visibility to private
+// @route PUT /api/playlist/:id/private
+// @access Private
+export const privateVisibility = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+    if(playlist.visibility.equals(false)){
+      res.status(400).json("Invalid Request: Playlist is already private.");
+    }
+    //check if playlist belongs to user
+    if (req.user._id.equals(playlist.userId)){
+      await playlist.updateOne({
+        $set: {visibility: false}
+    })
+    res.status(200).json("Updated visibility to private.");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+// @desc Add posts to playlist
+// @route PUT /api/playlist/:id/add
+// @access Private
+export const addPosts = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+    const { posts } = req.body
+    const postsArray = posts.split(",").map((post) => post.trim())
+    // validating all posts 
+    if (postsArray.length > 0 /*&& collaboratorsArray[0].trim().length > 0*/) {
+      postsArray.forEach(async (postId) => {
+        await Post.findById(postId)
+      })
+    }
+    //check if playlist belongs to user then add each new post to the playlist
+    if (req.user._id.equals(playlist.userId)){
+      postsArray.forEach(async (postId) => {
+        await playlist.updateOne({
+          $push: {content: postId}
+        })
+      })
+    res.status(200).json("Removed Posts.");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+// @desc Remove posts from playlist
+// @route PUT /api/playlist/:id/delete
+// @access Private
+export const rmPosts = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+    const { posts } = req.body
+    const postsArray = posts.split(",").map((post) => post.trim())
+    // validating all posts 
+    if (postsArray.length > 0 /*&& collaboratorsArray[0].trim().length > 0*/) {
+      postsArray.forEach(async (postId) => {
+        await Post.findById(postId)
+      })
+    }
+    //check if playlist belongs to user then add each new post to the playlist
+    if (req.user._id.equals(playlist.userId)){
+      postsArray.forEach(async (postId) => {
+        //specific error handling if post doesn't exist?
+        await playlist.updateOne({
+          $pull: {content: postId}
+        })
+      })
+      res.status(200).json("Removed Posts.");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+
+
+// @desc Add tags to playlist
+// @route PUT /api/playlist/:id/tags/add
+// @access Private
+export const addTags = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+    const { tags } = req.body
+    const tagsArray = tags.split(",").map((post) => post.trim())
+   
+    //check if playlist belongs to user then add each new post to the playlist
+    if (req.user._id.equals(playlist.userId)){
+      tagsArray.forEach(async (tag) => {
+        await playlist.updateOne({
+          $push: {tags: tag}
+        })
+    })
+    res.status(200).json("Added tags");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+
+// @desc Remove tags from playlist
+// @route PUT /api/playlist/:id/tags/delete
+// @access Private
+export const rmTags = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+    const { tags } = req.body
+    const tagsArray = tags.split(",").map((post) => post.trim())
+   
+    //check if playlist belongs to user then add each new post to the playlist
+    if (req.user._id.equals(playlist.userId)){
+      tagsArray.forEach(async (tag) => {
+        await playlist.updateOne({
+          $pull: {tags: tag}
+        })
+    })
+    res.status(200).json("Removed tags");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+
+// @desc Remove tags from playlist
+// @route PUT /api/playlist/:id/tags/delete
+// @access Private
+export const deletePlaylist = asyncHandler(async (req, res) => {
+  try{
+    const user = await User.findById(req.user._id)
+    const playlist = await Playlist.findById(req.params.id)
+
+    if (!user){
+      res.status(404).json("User not found.");
+    }
+    if (!playlist){
+      res.status(404).json("Playlist not found.");
+    }
+
+   
+    //check if playlist belongs to user then add each new post to the playlist
+    if (req.user._id.equals(playlist.userId)){
+        await user.updateOne({
+          $pull: {playlist: playlist}        
+        })
+        await playlist.deleteOne();//delete playlist from database
+
+    res.status(200).json("Deleted Playlist");
+    }
+    else{
+      res.status(403).json("Invalid Request. You don't have access to this playlist");
+    }
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+})
+
+
--- mashart/backend/routes/playlistRoutes.js
+++ mashart/backend/routes/playlistRoutes.js
@@ -1,6 +1,9 @@
 import express from "express"
 import {
-    createPlaylist
+    createPlaylist,
+    publicVisibility,
+    privateVisibility,
+    addPosts
 } from "../controllers/playlistController.js"
 
 import { protect } from "../middleware/authMiddleware.js"
@@ -8,7 +11,9 @@ import { protect } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 router.route("/create").post(protect, createPlaylist)
-
+router.route("/:id").put(protect,addPosts)
+router.route("/:id/public").put(protect,publicVisibility)
+router.route("/:id/private").put(protect,privateVisibility)
 
 export default router
 
