9d50a0618af5b9fb8f2fcdaf8a004c6063f7dfc4
Abdul Kader <abdulkaderm36@gmail.com>
2022-03-16 21:05:06 +0400

    added the leaveCollab

--- mashart/backend/controllers/collabController.js
+++ mashart/backend/controllers/collabController.js
@@ -88,6 +88,63 @@ export const joinCollab = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc Join a collab room
+// @route POST /api/collab/leave
+// @access Private
+export const leaveCollab = asyncHandler(async (req, res) => {
+  const { roomCode } = req.body
+
+  const user = await User.findById(req.user._id)
+  if (!user) {
+    throw new Error("User doesn't exist")
+  }
+
+  const collab = await Collab.findOne({ roomCode })
+
+  if (!collab) {
+    throw new Error("Invalid room code")
+  }
+
+  if (collab.users.length < 1) {
+    throw new Error("Room is empty")
+  }
+
+  if (!collab.users.includes(user._id)) {
+    throw new Error("Invalid Request")
+  }
+
+  await collab.updateOne({
+    $pull: { users: user._id },
+  })
+
+  const updatedCollab = await Collab.findOne({ roomCode }).populate(
+    "hostId",
+    "username"
+  )
+
+  if (updatedCollab) {
+    if (updatedCollab.users.length < 1) {
+      await Collab.findByIdAndDelete(updatedCollab._id)
+      res.status(200)
+      res.json({
+        message: "Successfully Deleted",
+      })
+    }
+
+    res.status(200)
+    res.json({
+      _id: updatedCollab._id,
+      roomCode: updatedCollab.roomCode,
+      hostId: updatedCollab.hostId,
+      users: updatedCollab.users,
+      content: updatedCollab.content,
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid collab data")
+  }
+})
+
 // @desc get a collab room's users
 // @route GET /api/collab/users/:roomCode
 // @access Private
--- mashart/backend/routes/collabRoutes.js
+++ mashart/backend/routes/collabRoutes.js
@@ -3,6 +3,7 @@ import {
   createCollab,
   getCollabUsers,
   joinCollab,
+  leaveCollab,
 } from "../controllers/collabController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
@@ -10,6 +11,7 @@ const router = express.Router()
 
 router.route("/create").post(protect, createCollab)
 router.route("/join").post(protect, joinCollab)
+router.route("/leave").post(protect, leaveCollab)
 router.route("/users/:roomCode").get(protect, getCollabUsers)
 
 export default router
