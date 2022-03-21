b8d05ea223e538d31566222f9a093a39b955ab87
Abdul Kader <abdulkaderm36@gmail.com>
2022-03-22 03:18:33 +0400

    create playlist

---
+++ mashart/frontend/src/actions/playlistActions.js
@@ -0,0 +1,51 @@
+import axios from "axios"
+import {
+  CREATE_PLAYLIST_FAIL,
+  CREATE_PLAYLIST_REQUEST,
+  CREATE_PLAYLIST_SUCCESS,
+  PLAYLIST_RESET,
+} from "../constants/playlistConstants"
+import { GET_USER_PLAYLISTS_RESET } from "../constants/userConstants"
+
+export const createPlaylist = (playlist) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CREATE_PLAYLIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post("/api/playlist/create", playlist, config)
+
+    dispatch({
+      type: CREATE_PLAYLIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: CREATE_PLAYLIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const createPlaylistReset = () => async (dispatch) => {
+  dispatch({
+    type: PLAYLIST_RESET,
+  })
+  dispatch({
+    type: GET_USER_PLAYLISTS_RESET,
+  })
+}
